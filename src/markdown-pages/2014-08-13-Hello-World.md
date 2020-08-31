709e7aa4d89ea920db1a40247b14c3b6b7af43a6
Deborah Gertrude Digges <deborah.digges@auth0.com>
2020-08-31 19:51:13 -0400

    Initial Commit

---
+++ src/markdown-pages/2014-08-13-Hello-World.md
@@ -0,0 +1,7 @@
+---
+slug: "/2014/08/13/Hello-World/"
+date: "2014-08-13"
+title : Hello World!
+---
+
+Hello! World?
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,35 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
+import Link from "gatsby-link";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({
+  data, 
+}) {
+  console.log(data);
+  const blogList = data.allMarkdownRemark.edges.map(edge => (<div>
+    <Link to={edge.node.frontmatter.slug}>{edge.node.frontmatter.title}</Link>
+    <p>{edge.node.frontmatter.date}</p>
+  </div>));
+  return (<>
+    {blogList}
+  </>);
+};
+
+export const pageQuery = graphql`
+{
+  allMarkdownRemark(
+    sort: { order: DESC, fields: [frontmatter___date] }
+    limit: 1000
+  ) {
+    edges {
+      node {
+        frontmatter {
+          slug,
+          title,
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+  }
 }
+`;
